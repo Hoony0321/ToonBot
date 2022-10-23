ee95ec19f6fc502c9f582ada20626e1f8aa84937
DeveloperHunny <eogns0321@gmail.com>
2022-10-23 16:23:22 +0900

    데이터 라벨링 로직

--- Crawling/labelingData.py
+++ Crawling/labelingData.py
@@ -7,27 +7,23 @@ writeFile = DATA_PATH + "/labels.csv"
 
 # csv 파일 변수
 rf = open(readFile, 'r', encoding='utf-8')
+next(rf)
 csvReader = csv.reader(rf)
 wf = open(writeFile, 'w', encoding='utf-8', newline='')
 csvWriter = csv.writer(wf)
 
-# webtoon 정보 불러오기
-
+columns = ["ID", "label"]
 rankIdx = 1 #랭크 인덱스 위치
 dayIdx = -1 #요일 인덱스 위치
-topCriteria = 35 #상위 웹툰 기준
+topCriteria = 30 #상위 웹툰 기준
 label = -1 #라벨링 변수
 
+# 웹툰 순위에 따른 라벨링( 1 : 흥행 성공 웹툰, 0 : 흥행 실패 웹툰 )
+csvWriter.writerow(columns)
 for data in csvReader:
-    if(data[rankIdx] <= topCriteria): label = 1
+    if(int(data[rankIdx]) <= topCriteria): label = 1
     else: label = 0
-
-    wf.write
-
-
-
-
-
+    csvWriter.writerow([data[0], label])
 
 rf.close()
 wf.close()
