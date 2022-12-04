2f2def0b4af1b12217f58da35f9708c6f299c26f
hoony0321 <eogns0321@gmail.com>
2022-12-04 14:29:14 +0900

    refactoring : class model 구성

--- KoBert/model.py
+++ KoBert/model.py
@@ -10,31 +10,87 @@ from KoBert.static import MODEL_PATH
 kobert_preprocessor_url = "https://tfhub.dev/jeongukjae/distilkobert_cased_preprocess/1"
 kobert_model_url = "https://tfhub.dev/jeongukjae/distilkobert_cased_L-3_H-768_A-12/1"
 
-def build_classifier_model():
-  text_input = tf.keras.layers.Input(shape=(), dtype=tf.string, name='text')
-  preprocessing_layer = hub.KerasLayer(kobert_preprocessor_url, name='preprocessing')
-  encoder_inputs = preprocessing_layer(text_input)
-  encoder = hub.KerasLayer(kobert_model_url, trainable=True, name='BERT_encoder')
-  outputs = encoder(encoder_inputs)
-  net = outputs['pooled_output']
-  net = tf.keras.layers.Dropout(0.1)(net)
-
-  ## 해당 부분에 데이터를 추가하면 될 듯(이미지 벡터화)
-  net = tf.keras.layers.Dense(1, activation=None, name='classifier')(net)
-  return tf.keras.Model(text_input, net)
-
 class ClassifierModel:
-  def __init__(self, version, epoch=100, batch_size=32):
+  def __init__(self, version, epoch=100, batch_size=32, validation_split=0.2):
     self.version = version
-    self.lr = 3e-5
+    self.lr = 0.000001
     self.epochs = epoch
     self.batch_size = batch_size
-    self.x_train = DataSetUtils.loadWebtoonData()
-    self.y_train = DataSetUtils.loadLabelData()
-    self.model = self._build_model()
+    self.validation_split = validation_split
+    self.x_train = None
+    self.y_train = None
+    self.model = None
     self.history = None
 
-  def _build_model(self):
+  def save_model(self):
+    self.model.save(MODEL_PATH + "/model_{}".format(self.version), include_optimizer=False)
+
+  def load_model(self):
+    self.model = tf.saved_model.load(MODEL_PATH + "/model_{}".format(self.version))
+
+
+  def build_concat_model(self):
+
+    # bert model
+    text_input = tf.keras.layers.Input(shape=(), dtype=tf.string, name='text')
+    preprocessing_layer = hub.KerasLayer(kobert_preprocessor_url, name='preprocessing')
+    encoder_inputs = preprocessing_layer(text_input)
+    encoder = hub.KerasLayer(kobert_model_url, trainable=True, name='BERT_encoder')
+    text_output = encoder(encoder_inputs)['pooled_output']
+    text_output = tf.keras.layers.BatchNormalization()(text_output)
+    text_output = tf.keras.layers.Dropout(0.1)(text_output)
+    # text_output = tf.keras.layers.Dense(32, activation='relu')(text_output)
+
+    bert_model = tf.keras.Model(inputs=text_input, outputs=text_output)
+
+    # image model
+    image_input = tf.keras.layers.Input(shape=(64,64,3), name='image')
+    normalization_layer = tf.keras.layers.experimental.preprocessing.Rescaling(1. / 255)
+    image_normalized = normalization_layer(image_input)
+    image_output = tf.keras.layers.Conv2D(32, 3, activation='relu')(image_normalized)
+    image_output = tf.keras.layers.MaxPooling2D()(image_output)
+    image_output = tf.keras.layers.Conv2D(32, 3, activation='relu')(image_output)
+    image_output = tf.keras.layers.MaxPooling2D()(image_output)
+    image_output = tf.keras.layers.Conv2D(32, 3, activation='relu')(image_output)
+    image_output = tf.keras.layers.MaxPooling2D()(image_output)
+    image_output = tf.keras.layers.Flatten()(image_output)
+    image_output = tf.keras.layers.Dense(64, activation='relu')(image_output)
+    image_output = tf.keras.layers.BatchNormalization()(image_output)
+    image_output = tf.keras.layers.Dropout(0.1)(image_output)
+
+
+    image_model = tf.keras.Model(inputs=image_input, outputs=image_output)
+
+    # sequential model
+    genre_input = tf.keras.layers.Input(shape=(13,), name='genre')
+
+    # concatenate model
+    concatenated = tf.keras.layers.concatenate([bert_model.output, genre_input, image_model.output])
+    concatenated = tf.keras.layers.Dense(32, activation='relu')(concatenated)
+    concatenated = tf.keras.layers.BatchNormalization()(concatenated)
+    concat_out = tf.keras.layers.Dense(1, activation=None, name='classifier')(concatenated)
+
+    concat_model = tf.keras.Model([text_input, genre_input, image_input], concat_out)
+
+    # 손실 함수 정의
+    loss = tf.keras.losses.BinaryCrossentropy(from_logits=True)
+    metrics = tf.metrics.BinaryAccuracy()
+
+    ##옵티마이저 정의
+    steps_per_epoch = int(len(self.x_train) / self.batch_size)
+    num_train_steps = steps_per_epoch * self.epochs
+    num_warmup_steps = int(0.1 * num_train_steps)
+    optimizer = optimization.create_optimizer(init_lr=self.lr,
+                                              num_train_steps=num_train_steps,
+                                              num_warmup_steps=num_warmup_steps,
+                                              optimizer_type='adamw')
+
+    # 모델 최적화 함수 & 손실 함수 설정
+    concat_model.compile(optimizer=optimizer, loss=loss, metrics=metrics)
+    # concat_model.summary()
+    self.model = concat_model
+
+  def build_model(self):
     text_input = tf.keras.layers.Input(shape=(), dtype=tf.string, name='text')
     preprocessing_layer = hub.KerasLayer(kobert_preprocessor_url, name='preprocessing')
     encoder_inputs = preprocessing_layer(text_input)
@@ -42,8 +98,9 @@ class ClassifierModel:
     outputs = encoder(encoder_inputs)
     net = outputs['pooled_output']
     net = tf.keras.layers.Dropout(0.1)(net)
+
     net = tf.keras.layers.Dense(1, activation=None, name='classifier')(net)
-    model = tf.keras.Model(text_input, net)
+    model = tf.keras.Model(inputs = text_input, outputs=net)
 
     # 손실 함수 정의
     loss = tf.keras.losses.BinaryCrossentropy(from_logits=True)
@@ -60,18 +117,24 @@ class ClassifierModel:
 
     # 모델 최적화 함수 & 손실 함수 설정
     model.compile(optimizer=optimizer, loss=loss, metrics=metrics)
-    return model
+    self.model = model
 
-  def train(self, validation_split=0.2):
-    history = self.model.fit(self.x_train, self.y_train, epochs=self.epochs, verbose=True, validation_split=validation_split)
+  def load_data(self):
+    self.x_train = [utils.load_brief_data(), utils.load_genre_data(), utils.load_image_data()]
+    self.y_train = utils.load_label_data()
+
+  def load_data_version0(self):
+    self.x_train = utils.load_brief_data()
+    self.y_train = utils.load_label_data()
+
+
+  def train(self):
+    history = self.model.fit(self.x_train, self.y_train, epochs=self.epochs, verbose=True, validation_split=self.validation_split)
     self.history = history
 
-    # 모델 저장
-    self.model.save(MODEL_PATH + "/model_{}".format(self.version), include_optimizer=False)
-    return history
-
-  def valid(self):
-    self.model.predict(self.x_train, split=1.0)
+  # 수정해야 함.
+  # def valid(self):
+  #   self.model.predict(self.x_train, split=1.0)
 
   def show_plot(self):
     history_dict = self.history.history
@@ -120,9 +183,3 @@ class ClassifierModel:
     plt.tight_layout(h_pad=4)
     plt.legend()
     plt.show()
-
-
-
-model = ClassifierModel("1.0.3",epoch=50,batch_size=32)
-model.train(validation_split=0.2)
-model.show_plot()
